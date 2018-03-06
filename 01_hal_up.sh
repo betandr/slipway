04079519d205d71cbbee8c106cdbdcc8fa0a1ca4
Beth Anderson <bet@andr.io>
2018-03-06 11:21:27 +0000

    Add Hal Up

---
+++ 01_hal_up.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+# This script is based on https://www.spinnaker.io/setup/quickstart/halyard-gke/
+# author: Beth Anderson (github.com/betandr)
+
+if test "$#" -ne 1; then
+    echo "Usage: sh 01_hal_up.sh 000 (where 000 is replaced with your index)"
+    exit 1
+fi
+
+echo "\n" \
+":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" \
+":: Warning: This script will provision infrastructure in the cloud and   ::\n" \
+":: will therefore cost real, cash money. Please be sure that you do want ::\n" \
+":: to proceed!                                                           ::\n" \
+":::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"
+
+while true; do
+    read -p "Create \`halyard-host-$1\` and service accounts?(y/n) " yn
+    case $yn in
+        [Yy]* )
+      		GCP_PROJECT=$(gcloud info --format='value(config.project)')
+      		HALYARD_SA=halyard-service-account-$1
+
+      		echo "---> creating Halyard host VM service account..."
+      		gcloud iam service-accounts create $HALYARD_SA \
+      		    --project=$GCP_PROJECT \
+      		    --display-name $HALYARD_SA
+
+      		HALYARD_SA_EMAIL=$(gcloud iam service-accounts list \
+      		    --project=$GCP_PROJECT \
+      		    --filter="displayName:$HALYARD_SA" \
+      		    --format='value(email)')
+
+      		gcloud projects add-iam-policy-binding $GCP_PROJECT \
+      		    --role roles/iam.serviceAccountKeyAdmin \
+      		    --member serviceAccount:$HALYARD_SA_EMAIL
+
+      		gcloud projects add-iam-policy-binding $GCP_PROJECT \
+      		    --role roles/container.admin \
+      		    --member serviceAccount:$HALYARD_SA_EMAIL
+
+          GCS_SA=gcs-service-account-$1
+
+          echo "---> creating GCS and GCR service accounts..."
+          gcloud iam service-accounts create $GCS_SA \
+              --project=$GCP_PROJECT \
+              --display-name $GCS_SA
+
+          GCS_SA_EMAIL=$(gcloud iam service-accounts list \
+              --project=$GCP_PROJECT \
+              --filter="displayName:$GCS_SA" \
+              --format='value(email)')
+
+          gcloud projects add-iam-policy-binding $GCP_PROJECT \
+              --role roles/storage.admin \
+              --member serviceAccount:$GCS_SA_EMAIL
+
+          HALYARD_HOST=$(echo halyard-host-$1 | tr '_.' '-')
+
+          echo "---> creating VM instance for halyard-host-$1..."
+          gcloud compute instances create $HALYARD_HOST \
+              --custom-cpu 1 \
+              --custom-memory 2304MB \
+              --project=$GCP_PROJECT \
+              --zone=europe-west2-c \
+              --scopes=cloud-platform \
+              --service-account=$HALYARD_SA_EMAIL \
+              --image-project=ubuntu-os-cloud \
+              --image-family=ubuntu-1404-lts
+
+          echo "gcloud compute ssh $HALYARD_HOST" \
+            "--project=$GCP_PROJECT" \
+            "--zone=europe-west2-c" \
+            "--ssh-flag=\"-L 9000:localhost:9000\"" \
+            "--ssh-flag=\"-L 8084:localhost:8084\"" > connect-to-halyard-host-$1.sh
+
+          echo "---> complete"
+
+          while true; do
+              read -p "---> SSH to instance now? (y/n) " yn
+              case $yn in
+                  [Yy]* )
+                    gcloud compute ssh $HALYARD_HOST \
+                      --project=$GCP_PROJECT \
+                      --zone=europe-west2-c \
+                      --ssh-flag="-L 9000:localhost:9000" \
+                      --ssh-flag="-L 8084:localhost:8084"
+          		      break;;
+                  [Nn]* )
+                    echo "---> you can connect later by running:\n" \
+                      "sh connect-to-halyard-host-$1.sh"
+                    exit;;
+                  * ) echo "Please answer yes or no.";;
+              esac
+          done
+
+      		break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# launch
-Scripts to automate a launch of a new Halyard VM and Spinnaker Cluster
+# Slipway: Automated Halyard and Spinnaker provisioning
+
+This script automates one use-case only; provisioning
+[Spinnaker](https://www.spinnaker.io/) on a Kubernetes cluster, controlled by
+an Ubuntu host VM running
+[Halyard](https://www.spinnaker.io/reference/halyard/). If this exact
+configuration is not what you want then the
+[Spinnaker Setup docs](https://www.spinnaker.io/setup/) might be a good starting
+point.
+
+## 1. Preamble
+Install [gcloud](https://cloud.google.com/sdk/downloads) SDK
+
+## 2. Provision Halyard Host VM
+
+### Configure gcloud
+```
+gcloud auth login
+```
+```
+gcloud config set project $your_project_name
+```
+
+### Get the Halyard host provisioning script
+```
+curl -O  https://raw.githubusercontent.com/betandr/slipway/master/01_hal_up.sh
+```
+
+### Run Halyard host provisioning script
+```
+# Change 001 to another label if you already have a halyard-host-001
+sh 01_hal_up.sh 001
+```
+
+_*Important: All further scripts are run from the Halyard Host VM!*_
+
+## 3. Install Halyard (from Halyard host VM!)
+
+## Configure and Deploy Spinnaker
+
+
+
+## Notes
+ Decom: Host VM, persistent disk (goes with VM),
+ gcs-service-account-INDEX@PROJECT.iam.gserviceaccount.com,
+ halyard-service-account-INDEX@PROJECT.iam.gserviceaccount.com
